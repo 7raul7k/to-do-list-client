8d7ce86cf9e70434d1def2a20a75f4a844febb73
7raul7k <balanescuraul@gmail.com>
2023-09-01 11:14:15 +0300

    add new-page-add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import {HttpClientModule} from "@angular/common/http";
+import { NewToDoListComponent } from './components/new-to-do-list/new-to-do-list.component';
+import {FormsModule} from "@angular/forms";
+import {MessagesModule} from "primeng/messages";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NewToDoListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MessagesModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
