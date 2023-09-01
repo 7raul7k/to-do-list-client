0f5fd2abcba37b21df24ef9638d38909291f99c9
7raul7k <balanescuraul@gmail.com>
2023-09-01 10:46:22 +0300

    add home

---
+++ src/app/service/todolist.service.ts
@@ -0,0 +1,37 @@
+import {Injectable, OnDestroy, OnInit} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {catchError, Observable, throwError} from "rxjs";
+import {ToDoListDTO} from "../models/api/ToDoListDTO";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodolistService implements OnInit, OnDestroy{
+
+
+  private url ="http://localhost:8080/api/v1/todo-list"
+  constructor(private http : HttpClient) { }
+
+  getTodoList():Observable<ToDoListDTO[]>{
+  return this.http.get<ToDoListDTO[]>(this.url + "/all").pipe(catchError(this.handleError));
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(()=>'Something bad happened; please try again later.');
+  };
+}
